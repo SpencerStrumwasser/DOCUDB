366f20fedb44163cc5f06b8f0673f122e5e69f97
Spencer Strumwasser <sstrumwa@caltech.edu>
2017-04-24 00:33:45 -0700

    Added the write document to data function

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import os
 
 
-FOUR_KILOBYTE = 4096
+READ_SIZE = 4096
 
 INT_SIZE = 8
 DOUBLE_SIZE = 16
@@ -22,46 +22,82 @@ BOOLEAN_SIZE = 1
 def search_memory_for_free(filename, size):
 	with open(filename, 'rb') as f:
 		start = 0
-		end = start + FOUR_KILOBYTE
+		end = start + READ_SIZE
+		#check if file has anything written
 		not_eof = (os.stat(filename).st_size != 0) 
-		print not_eof
 		while(not_eof):
 			f.seek(start)
+			#load a subset of data
 			data = f.read(end - start)
 			size_of_data = len(data)
-			print 'size data'
-			print size_of_data
-			if (size_of_data < FOUR_KILOBYTE):
+			#check if last loop
+			if (size_of_data < READ_SIZE):
 				not_eof = False
+			# go through "block" of data	
 			while(start <= size_of_data):
-				print 'clean'
-				print data[start]
 
 				dirty = int(data[start])
-				
-				print data[start + BOOLEAN_SIZE: BOOLEAN_SIZE+ INT_SIZE]
 				allocated = int(data[start + BOOLEAN_SIZE: start + BOOLEAN_SIZE + INT_SIZE].rstrip('\0'))
-				print allocated
 				if (dirty == 0) and (allocated >= size):
 					return start
 				else:
 					start += allocated
-			end = start + FOUR_KILOBYTE
+			end = start + READ_SIZE
 		return start
 
 
 
+def write_data_to_memory(filename, start, document):
+	with open(filename, 'r+b') as f:
+		f.seek(start)
+		# show it has not been "deleted"
+		f.write('1')
+		start += BOOLEAN_SIZE
+		f.seek(start)
+		# write how much is allocated
+		f.write(document.allocated_size)
+		start += INT_SIZE
+		f.seek(start)
+		# write how much is being used
+		f.write(document.filled_size)
+		start += INT_SIZE
+
+		values = document.values
+		for key in values:
+			# write column name lenght
+			f.seek(start)
+			f.write(values[key].col_name_len)
+			start += 1
+			# write column name
+			f.seek(start)
+			f.write(key)
+			start += values[key.col_name_len]
+			# write value type
+			f.seek(start)
+			f.write(values[key].val_type)
+			start += 1
+			# write value size
+			f.seek(start)
+			f.write(values[key].val_size)
+			start += INT_SIZE
+			# write value
+			f.seek(start)
+			f.write(values[key].val)
+			start += values[key].val_type
+	# return true to make sure it works
+	return True	
 
 
-a = search_memory_for_free('asdf.d', 100)
-print a
-with open('asdf.d', 'r+b') as f:
-	f.seek(a)
-	f.write('1')
-	f.seek(a+1)
-	f.write(bytesarray(100))
-	f.seek(a+ BOOLEAN_SIZE+INT_SIZE)
-	f.write('asfsdfsadf')
+
+# a = search_memory_for_free('asdf.d', 100)
+# print a
+# with open('asdf.d', 'r+b') as f:
+# 	f.seek(a)
+# 	f.write('1')
+# 	f.seek(a+1)
+# 	f.write(bytes(100000))
+# 	f.seek(a+ BOOLEAN_SIZE+INT_SIZE)
+# 	f.write('asfsdfsadf')
 # 	f.close()
 # a = search_memory_for_free('asdf.d', 30)
 # print a
